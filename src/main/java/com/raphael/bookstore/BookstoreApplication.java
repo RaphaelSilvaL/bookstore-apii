f4649696bc7a7712107c928a511ef87a1653774a
RaphaeSilvaL <rapha.43211@gmail.com>
2023-02-09 16:47:47 -0300

    quinta foi

--- src/main/java/com/raphael/bookstore/BookstoreApplication.java
+++ src/main/java/com/raphael/bookstore/BookstoreApplication.java
@@ -13,28 +13,14 @@ import com.raphael.bookstore.repositories.CategoriaRepository;
 import com.raphael.bookstore.repositories.LivroRepository;
 
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
+public class BookstoreApplication {
 	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
 
-		Categoria cat1 = new Categoria(null, "Informatica", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Loren ipsun", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-	}
 
 }
